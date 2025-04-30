dc19feea976f2f43213dc2e2905a17307d7aa430
steffi <sdstefficrazy93@gmail.com>
2025-04-30 12:12:22 +0530

    1

---
+++ springbootjdbc/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,123 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
+@RestController
+public class EmployeeController {
+	@Autowired
+    EmployeeRepository employeeRepository;
+
+    /**
+     * Get all the employees
+     *
+     * @return ResponseEntity
+     */
+    @GetMapping("/employees")
+    public ResponseEntity<List> getEmployees() {
+        try {
+            return new ResponseEntity<>(employeeRepository.getAllEmployees(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Get the employee by id
+     *
+     * @param id
+     * @return ResponseEntity
+     */
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long id) {
+        try {
+            //check if employee exist in database
+            Employee empObj = getEmpRec(id);
+
+            if (empObj != null) {
+                return new ResponseEntity<>(empObj, HttpStatus.OK);
+            }
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+
+    /**
+     * Delete Employee by Id
+     *
+     * @param id
+     * @return ResponseEntity
+     */
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<HttpStatus> deleteEmployeeById(@PathVariable("id") long id) {
+        try {
+            //check if employee exist in database
+            Employee emp = getEmpRec(id);
+
+            if (emp != null) {
+                employeeRepository.deleteEmployeeById(id);
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+    /**
+     * Method to get the employee record by id
+     *
+     * @param id
+     * @return Employee
+     */
+    private Employee getEmpRec(long id) {
+        Optional<Employee> empObj = Optional
+                .ofNullable
+                        (employeeRepository.getEmployeeById(id));
+
+        if (empObj.isPresent()) {
+            return empObj.get();
+        }
+        return null;
+    }
+
+
+    /**
+     * Create new employee
+     *
+     * @param employee
+     * @return ResponseEntity
+     */
+    @PostMapping("/employee")
+    public ResponseEntity<Employee> newEmployee(@RequestBody Employee employee) {
+        Employee newEmployee = employeeRepository
+                .save(Employee.builder()
+                        .name(employee.getName())
+                        .role(employee.getRole())
+                        .build());
+        return new ResponseEntity<>(newEmployee, HttpStatus.OK);
+    }
+}
