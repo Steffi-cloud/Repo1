a7070ce21e03940304007d5435c1fd1f68991594
steffi <sdstefficrazy93@gmail.com>
2025-05-11 18:16:46 +0530

    1

---
+++ springbootwithkafkasample/src/test/java/com/example/demo/SpringbootwithkafkasampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwithkafkasampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
