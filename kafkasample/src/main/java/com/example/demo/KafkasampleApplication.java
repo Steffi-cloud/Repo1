08cc7e65ab13f82beccdb4ac2ba17ed5996239a2
steffi <sdstefficrazy93@gmail.com>
2025-05-12 12:16:15 +0530

    1

---
+++ kafkasample/src/main/java/com/example/demo/KafkasampleApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@SpringBootApplication
+
+
+public class KafkasampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkasampleApplication.class, args);
+	}
+	
+	
+
+}
