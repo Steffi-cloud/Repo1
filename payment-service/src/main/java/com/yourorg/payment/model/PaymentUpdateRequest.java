44619c09aea050d634bf42426912b9c9243d2caa
unknown <sdstefficrazy93@gmail.com>
2025-10-18 10:13:05 +0530

    Remove mistakenly added top-level payment-service folder

--- payment-service/src/main/java/com/yourorg/payment/model/PaymentUpdateRequest.java
+++
@@ -1,8 +0,0 @@
-package com.yourorg.payment.model;
-
-public class PaymentUpdateRequest {
-	
-	private Integer id;
-	private String mode;
-
-}
--- payment-service/src/main/java/com/yourorg/payment/repository/SubscriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yourorg.payment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.yourorg.payment.model.SubscriptionRequest;
-
-public interface SubscriptionRepository extends JpaRepository<SubscriptionRequest, Integer> {
-
-}
